165be6381ee7dadfe11fcdbda64f8c2c9a49aa74
Vignesh <joshi.vignesh@gmail.com>
2022-08-13 17:46:09 +0530

    File and Folder structure
    File and Folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-// import { Route, Swtich } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
+import { ImportExport } from '@mui/icons-material';
 
-const App= () => (
-  <div>
+import  useStyles from './styles';
+
+import { Actors,  MovieInformation, Movies, NavBar, Profile } from './index';
+
+const App= () => { 
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
     <CssBaseline />
-    <h1>Hello World!</h1>
+    <NavBar></NavBar>
+    <main className={classes.content}>
+    <div className={classes.toolbar} />
+      <Routes>
+        <Route exact path="/" element="Movies"></Route>
+        <Route exact path="/actors/:id" element="Actors"></Route>
+        <Route exact path="/movieinformation/:id" element="MovieInformation"></Route>
+        <Route path="/movies" element="Movies" ></Route>
+      </Routes>
+    </main>
   </div>
-);
+  )
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
+  <BrowserRouter>
       <App />
+      </BrowserRouter>
   );
